5622e278f2f7856b2cf89e26ac1babe3af86b8bf
Andrew Fitzgibbon <awf@fitzgibbon.ie>
2023-03-25 22:36:02 +0000

    Add retry logic, -skip-existing, and -dir

--- scrap_neurIPS_papers.py
+++ scrap_neurIPS_papers.py
@@ -10,6 +10,8 @@
 """
 
 # Importing necessary libraries
+import os
+import time
 import requests
 import argparse
 import pandas as pd
@@ -19,8 +21,10 @@ from bs4 import BeautifulSoup, SoupStrainer
 
 # Initializing argeparse
 parser = argparse.ArgumentParser(description='Script to scrap NeurIPS Papers')
-parser.add_argument('-start', action="store", default=2020, dest="start_year", type=int, help='The start year to scrap the papers')
+parser.add_argument('-start', action="store", default=1987, dest="start_year", type=int, help='The start year to scrap the papers')
 parser.add_argument('-end', action="store", default=2022, dest="end_year", type=int, help='The end year to scrap the papers')
+parser.add_argument('-dir', action="store", default=None, dest="dir", type=str, help='Output directory to store per-paper csvs')
+parser.add_argument('-skip-existing', action="store_true", default=False, dest="skip_existing", help='Skip a paper if its csv is already there (needs -dir)')
 arguments = parser.parse_args()
 
 # Argeparse conditions
@@ -42,6 +46,8 @@ HEADERS = {
 }
 start_year = arguments.start_year
 end_year = arguments.end_year
+out_dir = arguments.dir
+skip_existing = arguments.skip_existing
 papers = []
 paper_authors = []
 
@@ -69,11 +75,54 @@ def get_all_hashes(url):
     except ConnectionError as error:
         print(error)
 
+def save_file(file_name, data, verbose=True):
+    """Converting a list of dicts to Pandas dataframe and saving it as a CSV File"""
+
+    df = pd.DataFrame(data)
+    df.to_csv(file_name, index=False, header=True)
+    if verbose:
+        print(f"Successfully saved {file_name}")
+
 
 def scrap_paper_and_authors(year, year_url, hashes):
     """Scrap papers and authors using extracted hashes"""
 
+    if out_dir:
+        year_dir = f"{out_dir}/{year}"
+        if not os.path.isdir(year_dir):
+            print(f"mkdir {year_dir}")
+            os.mkdir(year_dir)
+
     for paper_hash in tqdm(hashes):
+        if out_dir and skip_existing:
+          if os.path.exists(f"{year_dir}/{paper_hash}.csv"):
+              continue
+
+        if year <= 2019:
+          paper_url = f'{year_url}/file/{paper_hash}-Metadata.json'
+        else: 
+          paper_url = f'{BASE_URL_20}/{year}/hash/{paper_hash}-Abstract.html'
+
+        response = None
+        sleep_time = 1.0
+        while True:
+          try:
+             response = requests.get(paper_url, headers=HEADERS)
+          except Exception as e:
+             print(f'Caught {e}, sleeping and retrying')
+             time.sleep(sleep_time)
+             sleep_time *= 2
+             if sleep_time > 60:
+                sleep_time = 60
+
+          if response.status_code == 200:
+             if response.text == "Resource Not Found":
+                # This is not recoverable - it simply isn't there
+                raise RuntimeError(f"Bad url {response.url}")
+             break
+          
+
+        paper = {}
         if year <= 2019:
           paper_url = year_url + "/file/" + paper_hash + "-Metadata.json"
           response = requests.get(paper_url, headers=HEADERS)
@@ -81,14 +130,12 @@ def scrap_paper_and_authors(year, year_url, hashes):
               doc = response.json()
 
               # Extracting paper
-              paper = {}
               paper['source_id'] = doc['sourceid']
               paper['year'] = year_url.split("/")[-1]
               paper['title'] = doc['title']
               paper['abstract'] = doc['abstract'] if doc['abstract'] else None
               paper['full_text'] = doc['full_text']
               paper['pdf_url'] = None
-              papers.append(paper)
 
               # Extracting authors from a paper
               for author in doc['authors']:
@@ -101,7 +148,8 @@ def scrap_paper_and_authors(year, year_url, hashes):
           else:
               print("Couldn't complete the request.")
               break
-        else: # Post 2020
+        else: 
+          # Post 2020
           # https://papers.nips.cc/paper_files/paper/2022/hash/002262941c9edfd472a79298b2ac5e17-Abstract-Conference.html
           #  Contains metadata like 
           #     <meta name="citation_pdf_url" content="https://proceedings.neurips.cc/paper_files/paper/2022/file/002262941c9edfd472a79298b2ac5e17-Paper-Conference.pdf">
@@ -132,19 +180,14 @@ def scrap_paper_and_authors(year, year_url, hashes):
 
               paper['abstract'] = soup.find('div','col').text # includes title and authors too
               paper['full_text'] = None
-              papers.append(paper)
 
           else:
               print("Couldn't complete the request.")
               break
+        papers.append(paper)
+        if out_dir:
+          save_file(f"{year_dir}/{paper_hash}.csv", [paper], verbose=False)
 
-
-def save_file(file_name, data):
-    """Converting a list of dicts to Pandas dataframe and saving it as a CSV File"""
-
-    df = pd.DataFrame(data)
-    df.to_csv(file_name, index=False, header=True)
-    print(f"Successfully saved {file_name}")
 
 # Getting all conferences
 for year in tqdm(range(start_year, end_year+1)):
